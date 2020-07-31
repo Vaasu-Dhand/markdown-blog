2399bb0d6c6bceb151ecd9264a96c7f619a02834
Vaasu Dhand <dhandvaasu@gmail.com>
2020-07-30 22:32:28 -0400

    Deployed to Heroku

---
+++ routes/articles.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const Article = require('./../models/article')
+const router = express.Router()
+
+router.get('/new', (req, res) => {
+  res.render('articles/new', { article: new Article() })
+})
+
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article })
+  }
+  catch (e) {
+    console.error(e)
+  }
+})
+
+router.get('/:slug', async (req, res) => {
+  try {
+    const article = await Article.findOne({ slug: req.params.slug })
+    if (article == null) res.redirect('/')
+    res.render('articles/show', { article: article })
+  } catch (e) { console.error(e) }
+})
+
+router.post('/', async (req, res, next) => {
+try {
+  req.article = new Article()
+  console.log('Post Request Made')
+
+  next()
+} catch (e) {
+  console.error(e)
+}
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+try {
+  req.article = await Article.findById(req.params.id)
+  console.log('Put Request Made')
+
+  next()
+} catch (e) {
+  console.error(e)
+}
+
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+    console.log('Delete Request Made')
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    try {
+      article = await article.save()
+      res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+      console.error(e)
+      res.render(`articles/${path}`, { article: article })
+    }
+  }
+}
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const articleRouter = require("./routes/articles");
+const Article = require("./models/article");
+const methodOverride = require('method-override')
+
+require("dotenv").config();
+
+const app = express();
+const db = process.env.MongoURI;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: 'desc' })
+  res.render("articles/index", { articles });
+});
+
+// Connect to MongoDB
+mongoose
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+.then(() => console.log("MongoDb Connected..."))
+.catch((err) => console.error(err));
+
+app.use("/articles", articleRouter);
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, console.log(`Server started on Port: ${PORT}`));
